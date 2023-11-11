69dcd1e43015ef0827f96b64610d73b715bae4a2
Abu Dojana Tahmid <abudojana2001@gmail.com>
2023-11-11 08:08:55 +0600

    Update merge_sort.cpp

--- online 1 and 2 codes/merge_sort.cpp
+++ online 1 and 2 codes/merge_sort.cpp
@@ -78,3 +78,83 @@ int main() {
 Explanation:
 - For Question 1, the code sorts the entire array (`A`) and displays the sorted result.
 - For Question 2, the code sorts a specific subarray within the range of indices (`low` to `hi`) and displays the sorted subarray.
+
+Certainly! It sounds like you're asking for a modified version of the merge sort algorithm that utilizes a "three-way merge" for each step. Here's an implementation in C++:
+
+```cpp
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+vector<int> A;
+
+void three_way_merge(int low, int mid1, int mid2, int hi) {
+    int n1 = mid1 - low + 1;
+    int n2 = mid2 - mid1;
+    int n3 = hi - mid2;
+
+    vector<int> L(n1), M(n2), R(n3);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = A[low + i];
+
+    for (int i = 0; i < n2; i++)
+        M[i] = A[mid1 + 1 + i];
+
+    for (int i = 0; i < n3; i++)
+        R[i] = A[mid2 + 1 + i];
+
+    L.push_back(INT_MAX);
+    M.push_back(INT_MAX);
+    R.push_back(INT_MAX);
+
+    int i = 0, j = 0, k = 0;
+
+    for (int x = low; x <= hi; x++) {
+        if (L[i] <= M[j] && L[i] <= R[k]) {
+            A[x] = L[i];
+            i++;
+        } else if (M[j] <= L[i] && M[j] <= R[k]) {
+            A[x] = M[j];
+            j++;
+        } else {
+            A[x] = R[k];
+            k++;
+        }
+    }
+}
+
+void three_way_merge_sort(int low, int hi) {
+    if (low < hi) {
+        int n = hi - low + 1;
+
+        int mid1 = low + n / 3;
+        int mid2 = low + 2 * n / 3;
+
+        three_way_merge_sort(low, mid1);
+        three_way_merge_sort(mid1 + 1, mid2);
+        three_way_merge_sort(mid2 + 1, hi);
+
+        three_way_merge(low, mid1, mid2, hi);
+    }
+}
+
+int main() {
+    A = {5, 10, -3, 8, 15, 0, 1, -8, -1, -10, 12, -9, 14, 3, -2, -6, 2, -4, 7, 6, 16, 19, -5, 18, -7, 17, -2, 13, -4, -3, -1, 20};
+    three_way_merge_sort(0, A.size() - 1);
+
+    cout << "Sorted Array with Three-Way Merge: ";
+    for (auto i : A)
+        cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
+```
+
+Explanation:
+- The `three_way_merge` function is an adaptation of the merge step in the merge sort algorithm, allowing for three-way merging of subarrays.
+- The `three_way_merge_sort` function recursively divides the array into three parts and performs the three-way merge at each step.
+- The main function initializes the array, calls the three-way merge sort, and displays the sorted array.
